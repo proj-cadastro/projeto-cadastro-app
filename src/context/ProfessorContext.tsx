f0fa3047bad2bad553530dcd8a6cbc6649c75b55
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-26 15:31:35 -0300

    feat: add Course and Professor context providers for global state management

---
+++ src/context/ProfessorContext.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { createContext, useContext, useEffect, useState, ReactNode, useCallback } from 'react'
+import { Professor } from "../types/professor"
+import { getProfessors } from '../services/professors/ProfessorService'
+
+
+type ProfessorContextType = {
+    professors: Professor[]
+    loading: boolean
+    refreshProfessoresData: () => void
+}
+
+export const ProfessorContext = createContext<ProfessorContextType | undefined>(undefined)
+
+export const ProfessorProvider = ({ children }: { children: ReactNode }) => {
+    const [professors, setProfessors] = useState<Professor[]>([])
+    const [loading, setLoading] = useState(true)
+
+    const fetchData = useCallback(async () => {
+        try {
+            const professoresData = await getProfessors()
+            setProfessors(professoresData)
+        } catch (error: any) {
+            console.error(error.response?.error?.messagem || "Erro ao carregar professores")
+        } finally {
+            setLoading(false)
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
+
+    return (
+        <ProfessorContext.Provider value={{ professors, loading, refreshProfessoresData: fetchData }}>
+            {children}
+        </ProfessorContext.Provider>
+    )
+}
+
+export const useProfessor = () => {
+    const context = useContext(ProfessorContext)
+
+    if (!context) throw new Error("useProfessor must be used within ProfessorProvider")
+
+    return context
+}
